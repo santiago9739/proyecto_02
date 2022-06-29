8789477214b952a147daf970606ea89d45f5883b
kfbastidas <kevithchi@unicauca.edu.co>
2022-06-28 23:46:16 -0500

    se edito la categoria

--- src/app/components/dashboard/categorias/categorias.component.ts
+++ src/app/components/dashboard/categorias/categorias.component.ts
@@ -5,13 +5,16 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Categoria } from '@data/interfaces/categoria';
 import { CategoriaService } from 'app/services/categoria.service';
 import { EscenarioService } from 'app/services/escenario.service';
 import { FileService } from 'app/services/file.service';
+import { RemoveComponent } from './remove/remove.component';
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
@@ -47,7 +50,10 @@ export class CategoriasComponent implements OnInit {
     private _escenarios: EscenarioService,
     private fb: FormBuilder,
     private _file: FileService,
-    private _categorias: CategoriaService
+    private _categorias: CategoriaService,
+    private elementRef: ElementRef,
+    private _snackBar: MatSnackBar,
+    public dialog: MatDialog,
   ) {
     this.form = fb.group({
       nombre: this.nombreControl,
@@ -70,6 +76,7 @@ export class CategoriasComponent implements OnInit {
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.elementRef.nativeElement.focus();
   }
 
   ngOnInit(): void {}
@@ -87,10 +94,11 @@ export class CategoriasComponent implements OnInit {
       next: (result) => {
         if (result != null) {
           this.updateCategorias();
+          this.mensajeConfirmacionCrear();
         }
+        this.limpiarInfoCategoria();
       },
     });
-    console.log('entro al metodo crear');
   }
 
   updateCategorias() {
@@ -111,36 +119,59 @@ export class CategoriasComponent implements OnInit {
         next: (resultEdi) => {
           if (resultEdi != null) {
             this.updateCategorias();
+            this.mensajeConfirmacionEditar();
           }
+          this.limpiarInfoCategoria();
         },
       });
     console.log(this.categoria);
   }
 
   eliminar() {
-    this._categorias.eliminarCategoria(this.categoria).subscribe({
-      next: (result) => {
-        if (result == true) {
-          this.updateCategorias();
-        }
-      },
-    });
-    console.log('entro al metodo eliminar');
+    this.dialog
+      .open(RemoveComponent, {
+        data: { dato: this.categoria, method: 'Remove' },
+      })
+      .afterClosed()
+      .subscribe((result: any) => {
+        if (result !== undefined)
+          if (result.status == true) {
+            this._categorias.eliminarCategoria(this.categoria).subscribe({
+              next: (result) => {
+                if (result == true) {
+                  this.updateCategorias();
+                  this.mensajeConfirmacionEliminar();
+                }
+              },
+            });
+            this.limpiarInfoCategoria();
+          }
+      }); 
+    
   }
   //#endregion
 
   //#region Metodos
 
-  cargarInfoCategoria(categoria: Categoria) {
-    this.categoria = categoria;
-    this.nombreControl.setValue(categoria.cat_nombre);
-    this.nombreControl.addValidators([Validators.min(1), Validators.required]);
+  limpiarInfoCategoria()
+  {
+    this.categoria = {cat_descripcion:"",cat_foto:null,cat_nombre:""}
+    this.cargarInfoCategoria(this.categoria);
+    this.statusAgregar = this.statusEditar = this.statusEliminar = false;
+    
+  }
 
-    this.descripcionControl.setValue(categoria.cat_descripcion);
+  cargarInfoCategoria(categoria: Categoria) {
+    
+    this.categoria = categoria;
+    this.nombreControl.addValidators([Validators.min(1), Validators.required]);
+    this.nombreControl.setValue(categoria.cat_nombre);
+
     this.descripcionControl.addValidators([
       Validators.min(1),
       Validators.required,
     ]);
+    this.descripcionControl.setValue(categoria.cat_descripcion);
 
     if (categoria.cat_foto != null)
       this.retrievedImage = 'data:image/jpeg;base64,' + categoria.cat_foto;
@@ -148,6 +179,7 @@ export class CategoriasComponent implements OnInit {
   }
 
   informacionColumna(p_cat: any) {
+    this.onImageDefault();
     this.clickedRows.push(p_cat);
     this.cargarInfoCategoria(p_cat);
     this.statusEditar = true;
@@ -229,6 +261,7 @@ export class CategoriasComponent implements OnInit {
   //Gets called when the user selects an image
   onFileChanged(event: any) {
     //Select File
+    //this.elementInput.nativeElement.value = '';
     const reader = new FileReader();
     reader.onload = (e: any) => {
       //this.escenario.esc_foto = e.target.result.split('base64,')[1];
@@ -236,6 +269,35 @@ export class CategoriasComponent implements OnInit {
       this.retrievedImage = 'data:image/jpeg;base64,' + this.foto;
     };
     reader.readAsDataURL(event.target.files[0]);
+  }
+  //#endregion
+
+  //#region mensajes
+  mensajeConfirmacionCrear(): void {
+    this.openSnackBar('Se registro la categoria correctamente...', '', {
+      duration: 2000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+
+  mensajeConfirmacionEliminar(): void {
+    this.openSnackBar('La categoria se elimino correctamente', '', {
+      duration: 2000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+  mensajeConfirmacionEditar(): void {
+    this.openSnackBar('La categoria se actualizo correctamente', '', {
+      duration: 2000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+
+  openSnackBar(message: string, action: string, f: any) {
+    this._snackBar.open(message, action, f);
   }
   //#endregion
 
